882022cf39121b98463994898ab53402da47e58a
Tharini Mandya Srinivas <tharini.srinivas@collaboration-factory.de>
2022-04-09 20:31:59 +0200

    Initial commit

---
+++ src/com/company/automation/Main.java
@@ -0,0 +1,9 @@
+package com.company.automation;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+    }
+}
